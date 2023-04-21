a77636fa312f2ced25132b965965501787ba2c95
Like-A-Hoss <hapleswithdice@gmail.com>
2023-04-21 15:50:23 -0500

    Added random name selection

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send("Kraude, Jordan");
+  res.send(pick_name(students));
 });
 const port = 3000
 app.listen(process.env.port || port);
 console.log('Web Server is listening at port '+ (process.env.port || 3000));
+
+function pick_name(names){
+    const random = Math.floor(Math.random()*names.length);
+    const studnet_name = names[random];
+}
+const students = ["Kraude, Jordan", "Cutchen, Eli", "Thompson, Alicia", "Arndt, Diane", "Dickson, Robin", "Thomas, McKay", "Oku, James", "Hoskins, Nathan" ];
