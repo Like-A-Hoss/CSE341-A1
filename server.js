f8fb35c0147fee7f6303da2d3460ea5497c8677e
Like-A-Hoss <hapleswithdice@gmail.com>
2023-04-21 15:18:16 -0500

    updated server.js to send name of a classmate

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req,res) => {
-  res.send("Hello");
+  res.send("Kraude, Jordan");
 });
 const port = 3000
 app.listen(process.env.port || port);
