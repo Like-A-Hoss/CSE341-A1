0e461ad8a911907d0eb300c3494d18e8525273ed
Like-A-Hoss <hapleswithdice@gmail.com>
2023-04-21 15:51:38 -0500

    fixed pick name function

--- server.js
+++ server.js
@@ -11,5 +11,6 @@ console.log('Web Server is listening at port '+ (process.env.port || 3000));
 function pick_name(names){
     const random = Math.floor(Math.random()*names.length);
     const studnet_name = names[random];
+    return studnet_name;
 }
 const students = ["Kraude, Jordan", "Cutchen, Eli", "Thompson, Alicia", "Arndt, Diane", "Dickson, Robin", "Thomas, McKay", "Oku, James", "Hoskins, Nathan" ];
