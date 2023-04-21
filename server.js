839667d82c2899ca49288fdc59fde05682afe42c
Like-A-Hoss <hapleswithdice@gmail.com>
2023-04-21 17:12:41 -0500

    Fixed details

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 app.get('/', (req,res) => {
   res.send("Nathan S. Hoskins");
 });
-const port = 3000
+const port = 3000;
 app.listen(process.env.port || port);
-console.log('Web Server is listening at port '+ (process.env.port || 3000));
+console.log('Web Server is listening at port '+ (process.env.port || port));
 
