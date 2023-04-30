31dfc7aee987cc397e803566c1d11f7aa774cda3
Like-A-Hoss <hapleswithdice@gmail.com>
2023-04-29 21:34:35 -0500

    adding database connection

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const lesson1_controler = require('./controlers/lesson1');
+//const enviroment = require('custom-env').env('info');
+const db_conn = require('./models/db_connection');
 
 app.get('/', lesson1_controler.meRoute);
 app.get('/random',lesson1_controler.randomRoute);
@@ -14,3 +16,4 @@ console.log('Web Server is listening at port '+ (process.env.port || port));
 //    const studnet_name = names[random];
 //    return studnet_name;
 //}
+db_conn.run()
