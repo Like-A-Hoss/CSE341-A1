6cca43ab0fb7ff2c57fa3945ddfe1f44b57371af
Like-A-Hoss <hapleswithdice@gmail.com>
2023-05-12 23:36:22 -0500

    Swagger should now be working. Let's all pray.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const swaggerUi = require('swagger-ui-express');
+const specs = require('../swagger.json');
 
 router.use('/', require('./me'));
 router.use('/random', require('./random'))
 router.use('/contacts', require('./contacts'));
 router.use('/search', require('./search'));
+router.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
 //router.use('/post', require('./contacts_new'));
 //router.use('/byId', )
 //router.use('/id-to-modify', require('./contacts_update'))
--- swagger.js
+++ swagger.js
@@ -1,22 +1,36 @@
-const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerAutogen = require('swagger-autogen')()
 
-const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "Your API Title",
-      version: "1.0.0",
-      description: "API documentation for your API",
-    },
-    servers: [
-      {
-        url: "http://localhost:3000",
-      },
-    ],
+const outputFile = './swagger.json'
+const endpointsFiles = ['./routes/*.js'] // Your route files path
+
+const doc = {
+  info: {
+    version: "1.0.0",
+    title: "Your API Title",
+    description: "API documentation for your API"
   },
-  apis: ["./routes/*.js"], // Your route files path
-};
+  host: [
+    {
+        localUrl: "localhost:3000",
+        productionUrl: "https://cse341-spring23-3b0l.onrender.com"
+    }],// Your API host URL
+  basePath: "/",
+  schemes: ['http'],
+  consumes: ['application/json'],
+  produces: ['application/json'],
+  tags: [ // Your tags definition
+    {name: 'Contacts', description: 'API for Nathan S. Hoskins Contacts'}
+  ],
+  securityDefinitions: { // Your security definitions
+    JWT: {
+      type: 'none',
+      in: 'none',
+      name: 'none',
+      description: "",
+    }
+  }
+}
 
-const specs = swaggerJsDoc(options);
-
-module.exports = specs;
+swaggerAutogen(outputFile, endpointsFiles, doc).then(() => {
+  require('./routes/index') // Your Express.js or Koa.js app file path
+})
